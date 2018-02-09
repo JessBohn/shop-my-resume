498a8287ee57778f65acdd84ceb3d208ed105c91
Jessica Bohn <bohn.16@wright.edu>
2018-02-08 18:27:39 -0600

    Add data types needed for the Skill model

--- db/migrate/20180201065911_create_skills.rb
+++ db/migrate/20180201065911_create_skills.rb
@@ -1,6 +1,10 @@
 class CreateSkills < ActiveRecord::Migration[5.0]
   def change
     create_table :skills do |t|
+      t.string :title
+      t.string :description
+      t.string :image
+      t.integer :experience
 
       t.timestamps
     end
