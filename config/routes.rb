37324396d29e4e1abbca991b70b061d7bcfd713d
Jessica Bohn <bohn.16@wright.edu>
2018-02-12 23:03:14 -0600

    Fix Pages controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'welcome' => 'pages#home'
+  get 'pages/index'
+
+  get 'welcome' => 'pages#index'
   get '/skills' => 'skills#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
